703a51a2f94a78dcdd324e004215540f291776be
Loveink <106765014+Loveink@users.noreply.github.com>
2023-08-28 19:32:20 +0300

    add trending label and see all

--- BestRecipes/Screens/Home/Controllers/HomeViewController.swift
+++ BestRecipes/Screens/Home/Controllers/HomeViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-  private   let searchBar = SearchBar()
+  private let searchBar = SearchBar()
   private let collectionView = TrendingCollectionView()
     
    private let scrollView: UIScrollView = {
@@ -17,21 +17,12 @@ class HomeViewController: UIViewController {
         return scrollView
     }()
 
-    
-    var mainLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .black
-        label.textAlignment = .left
-        label.font = UIFont.systemFont(ofSize: 35)
-        label.text = """
-                     Get amazing recipes
-                     for cooking
-                     """
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.numberOfLines = 2
-        
-        return label
-    }()
+  var mainLabel = UILabel.makeLabelForCells(text: "Get amazing recipes for cooking", font: .poppinsSemiBold(size: 24), textColor: .black)
+  var trendingLabel = UILabel.makeLabelForCells(text: "Trending now 🔥", font: .poppinsSemiBold(size: 20), textColor: .black)
+
+  var seeAllButtonTrend = SeeAllButton()
+  var seeAllButtonRecipe = SeeAllButton()
+  var seeAllButtonCreators = SeeAllButton()
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -62,6 +53,8 @@ class HomeViewController: UIViewController {
     func setupCollectionView() {
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.addSubview(collectionView)
+        scrollView.addSubview(trendingLabel)
+        scrollView.addSubview(seeAllButtonTrend)
     }
     
   
@@ -70,26 +63,31 @@ class HomeViewController: UIViewController {
      
       
     NSLayoutConstraint.activate([
-        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-        
-        
-        searchBar.view.topAnchor.constraint(equalTo:  scrollView.topAnchor),
-        searchBar.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-        searchBar.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-        
-        mainLabel.topAnchor.constraint(equalTo: searchBar.searchBar.bottomAnchor, constant: 20),
-        mainLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-        mainLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        mainLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 50),
-        
-        
-        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-        collectionView.topAnchor.constraint(equalTo: mainLabel.bottomAnchor, constant: 50),
-      collectionView.heightAnchor.constraint(equalToConstant: 250) 
+      scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+      scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+      searchBar.view.topAnchor.constraint(equalTo:  mainLabel.bottomAnchor, constant: 8),
+      searchBar.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      searchBar.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
+      mainLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: -20),
+      mainLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      mainLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+      mainLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 50),
+
+      trendingLabel.topAnchor.constraint(equalTo: searchBar.view.bottomAnchor, constant: 8),
+      trendingLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 8),
+      trendingLabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 150),
+
+      seeAllButtonTrend.topAnchor.constraint(equalTo: searchBar.view.bottomAnchor, constant: 8),
+      seeAllButtonTrend.trailingAnchor.constraint(equalTo: searchBar.searchBar.trailingAnchor, constant: 8),
+
+      collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+      collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+      collectionView.topAnchor.constraint(equalTo: trendingLabel.bottomAnchor, constant: 8),
+      collectionView.heightAnchor.constraint(equalToConstant: 250)
     ])
   }
   
