c88028cb23f7484080d14645b4481667c526a854
Ivan (Vanopr) <mp3mp61@gmail.com>
2023-08-28 16:46:06 +0300

    Add Search, Add Model File

---
+++ BestRecipes/Help/LetAndVarBrain.swift
@@ -0,0 +1,12 @@
+//
+//  LetAndVarBrain.swift
+//  BestRecipes
+//
+//  Created by Vanopr on 28.08.2023.
+//
+
+import Foundation
+import UIKit
+
+var lastVisitedViewController: UIViewController?
+var titleOfView = ""
--- BestRecipes/Screens/Home/Controllers/HomeViewController.swift
+++ BestRecipes/Screens/Home/Controllers/HomeViewController.swift
@@ -8,24 +8,87 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-  
+  private   let searchBar = SearchBar()
   private let collectionView = TrendingCollectionView()
+    
+   private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.isPagingEnabled = true
+        scrollView.showsHorizontalScrollIndicator = false
+        return scrollView
+    }()
+
+    
+    var mainLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .left
+        label.font = UIFont.systemFont(ofSize: 35)
+        label.text = """
+                     Get amazing recipes
+                     for cooking
+                     """
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 2
+        
+        return label
+    }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
     view.backgroundColor = .white
+    setupScrollView()
+    setupSearchBar()
+    setupNameView()
+    setupCollectionView()
     setupConstraints()
     loadTrendingRecipes()
   }
+    func setupScrollView() {
+        view.addSubview(scrollView)
+        scrollView.contentSize = CGSize(width: .zero, height: 1400)
+    }
+    func setupSearchBar() {
+        searchBar.searchBar.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(searchBar.view)
+    }
+    
+    func setupNameView() {
+        scrollView.addSubview(mainLabel)
+    }
+    
+    func setupCollectionView() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(collectionView)
+    }
+    
   
   private func setupConstraints() {
-    view.addSubview(collectionView)
-    collectionView.translatesAutoresizingMaskIntoConstraints = false
+      
+     
+      
     NSLayoutConstraint.activate([
-      collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-      collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-      collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        
+        
+        searchBar.view.topAnchor.constraint(equalTo:  scrollView.topAnchor),
+        searchBar.view.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
+        searchBar.view.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20),
+        
+        mainLabel.topAnchor.constraint(equalTo: searchBar.searchBar.bottomAnchor, constant: 20),
+        mainLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
+        mainLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+        mainLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 50),
+        
+        
+        collectionView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 10),
+        collectionView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -10),
+        collectionView.topAnchor.constraint(equalTo: mainLabel.bottomAnchor, constant: 50),
       collectionView.heightAnchor.constraint(equalToConstant: 250) 
     ])
   }
