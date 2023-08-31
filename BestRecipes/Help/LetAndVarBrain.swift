eb86e8ae0085e4780bf95bec9891f6d7dd4c4957
Ivan (Vanopr) <mp3mp61@gmail.com>
2023-08-31 09:08:45 +0300

    Revert "data model"
    This reverts commit cb08f9dd4c3cbe409268138a95e532e83c591aaa.

--- BestRecipes/Help/LetAndVarBrain.swift
+++ BestRecipes/Help/LetAndVarBrain.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import UIKit
-import CoreData
 
 var lastVisitedViewController: UIViewController?
 var titleOfView = ""
+var imageProfileViewSaved = UIImage(named: "ProfilePicture1")
