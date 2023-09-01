effb318d03bf715016d65f5851de53ac4ebf0567
vtagilov <v.tagilov@mail.ru>
2023-09-01 13:33:43 +0300

    Add nav Controller to SeeAll VC

--- BestRecipes/Screens/See all/Controllers/SeeAllViewController.swift
+++ BestRecipes/Screens/See all/Controllers/SeeAllViewController.swift
@@ -5,4 +5,53 @@
 //  Created by Александра Савчук on 26.08.2023.
 //
 
-import Foundation
+import UIKit
+
+class SeeAllViewController: UIViewController {
+    
+    let navigationBar = CustomNavigationBar()
+    
+    
+    init(title: String) {
+        super.init(nibName: nil, bundle: nil)
+        navigationBar.titleOfViewLabel.text = title
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        setupNavBar()
+        
+        setConstraints()
+    }
+    
+    
+    private func setupNavBar() {
+        navigationBar.view.translatesAutoresizingMaskIntoConstraints = false
+        addChild(navigationBar)
+        view.addSubview(navigationBar.view)
+        navigationBar.didMove(toParent: self)
+        
+        NSLayoutConstraint.activate([
+            navigationBar.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            navigationBar.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            navigationBar.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            navigationBar.view.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+    }
+    
+    
+    private func setConstraints() {
+        
+        
+    }
+    
+    
+}
