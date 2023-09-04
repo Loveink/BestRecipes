adb0b753dfd0b74cb0993e446e40f0c5b6abb9ad
Timur Sattarov <sattarovtimurbek@gmail.com>
2023-09-04 10:32:35 +0300

    Add scroll and some code

--- BestRecipes/Screens/Recipe detail/Views/RecipeDetailView.swift
+++ BestRecipes/Screens/Recipe detail/Views/RecipeDetailView.swift
@@ -9,36 +9,29 @@ import UIKit
 
 class RecipeDetailView: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    
     var ingridients = ["Тартилия", "Лук", "Помидор", "Огурцы", "Курица"]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.dataSource = self
-        tableView.delegate = self
-        subview()
-        setupConstraints()
-        tableView.register(CustomCellRecipe.self, forCellReuseIdentifier: "CustomCellRecipe")
-        tableView.separatorStyle = .none
-    }
-    
-    
-    
-    //MARK: - Add subview func
-    
-    private func subview() {
-        view.addSubview(stackView)
-        view.backgroundColor = .white
-        
-        stackView.addArrangedSubview(textLabel)
-        stackView.addArrangedSubview(imageFood)
-        stackView.addArrangedSubview(recipeLabel)
-        stackView.addArrangedSubview(horizontalStackView)
-        stackView.addArrangedSubview(tableView)
-        
-        
-    }
+    let navigationBar = CustomNavigationBar()
     
     //MARK: - UI elements
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private let cartButton: UIButton = {
+        let cartButton = UIButton()
+        cartButton.setTitle("Add to cart", for: .normal)
+        cartButton.backgroundColor = .red
+        cartButton.layer.cornerRadius = 15
+        cartButton.titleLabel?.font = .poppinsSemiBold(size: 20)
+        cartButton.titleLabel?.textColor = .white
+        cartButton.isEnabled = true
+        return cartButton
+    }()
     
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView()
@@ -60,7 +53,7 @@ class RecipeDetailView: UIViewController, UITableViewDataSource, UITableViewDele
         let textLabel = UILabel()
         textLabel.text = "Шаурма"
         textLabel.textAlignment = .center
-        textLabel.font = .poppinsRegular(size: 24)
+        textLabel.font = .poppinsRegular(size: 18)
         return textLabel
     }()
     
@@ -68,14 +61,30 @@ class RecipeDetailView: UIViewController, UITableViewDataSource, UITableViewDele
         let imageFood = UIImageView()
         imageFood.image = UIImage(named: "image")
         imageFood.contentMode = .scaleAspectFit
-//        imageFood.heightAnchor.constraint(equalToConstant: 206).isActive = true
+        
         return imageFood
+    }()
+    
+    private lazy var likeLabel: UILabel = {
+        let likeLabel = UILabel()
+        likeLabel.text = "150 Likes"
+        likeLabel.textAlignment = .left
+        likeLabel.font = .poppinsRegular(size: 12)
+        return likeLabel
+    }()
+    
+    private lazy var instructionLabel: UILabel = {
+        let instructionLabel = UILabel()
+        instructionLabel.text = "Instruction"
+        instructionLabel.textAlignment = .left
+        instructionLabel.font = .poppinsSemiBold(size: 16)
+        return instructionLabel
     }()
     
     private lazy var recipeLabel: UILabel = {
         let recipeLabel = UILabel()
-        recipeLabel.text = "Подготовь ингредиенты: 1. Маринованное мясо (курица, говядина или баранина) - нарежь его тонкими полосками и предварительно промаринуй с специями. 2. Овощи (помидоры, огурцы, лук, зелень) - нарежь их полосками или кубиками.."
-        recipeLabel.font = .poppinsRegular(size: 17)
+        recipeLabel.text = "Подготовь ингредиенты: 1. Маринованное мясо (курица, говядина или баранина) - нарежь его тонкими полосками и предварительно промаринуй с специями. 2. Овощи (помидоры, огурцы, лук, зелень) - нарежь их полосками или кубиками."
+        recipeLabel.font = .poppinsRegular(size: 14)
         recipeLabel.numberOfLines = 0
         return recipeLabel
     }()
@@ -83,14 +92,15 @@ class RecipeDetailView: UIViewController, UITableViewDataSource, UITableViewDele
     private lazy var ingridentsLabel: UILabel = {
         let ingridentsLabel = UILabel()
         ingridentsLabel.text = "ingridents"
-        ingridentsLabel.font = .poppinsRegular(size: 15)
+        ingridentsLabel.font = .poppinsSemiBold(size: 16)
         return ingridentsLabel
     } ()
     
     private lazy var itemLabel: UILabel = {
         let itemLabel = UILabel()
         itemLabel.text = "5 items"
-        itemLabel.font = .poppinsRegular(size: 15)
+        itemLabel.font = .poppinsRegular(size: 16)
+        itemLabel.textAlignment = .right
         
         return itemLabel
     } ()
@@ -102,21 +112,81 @@ class RecipeDetailView: UIViewController, UITableViewDataSource, UITableViewDele
     }()
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(CustomCellRecipe.self, forCellReuseIdentifier: "CustomCellRecipe")
+        tableView.separatorStyle = .none
+        setupNavBar()
+        subview()
+        setupConstraints()
+        cartButton.addTarget(self, action: #selector(cartButtonPressed), for: .touchUpInside)
+        
+    }
+    
+    
+    //MARK: - Button
+    @objc func cartButtonPressed() {
+        print("chartButton pressed")
+    }
+    
+    //MARK: - Add subview func
+    
+    private func subview() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
+        //        view.addSubview(stackView)
+        view.backgroundColor = .white
+        
+        stackView.addArrangedSubview(textLabel)
+        stackView.addArrangedSubview(imageFood)
+        stackView.addArrangedSubview(likeLabel)
+        stackView.addArrangedSubview(instructionLabel)
+        stackView.addArrangedSubview(ingridentsLabel)
+        stackView.addArrangedSubview(recipeLabel)
+        stackView.addArrangedSubview(horizontalStackView)
+        stackView.addArrangedSubview(tableView)
+        stackView.addArrangedSubview(cartButton)
+        
+        
+    }
+    
     // MARK: -  Constrains
     func setupConstraints() {
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -2),
+            navigationBar.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            navigationBar.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            navigationBar.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            navigationBar.view.heightAnchor.constraint(equalToConstant: 50),
             
-            imageFood.widthAnchor.constraint(equalToConstant: 343),
-            imageFood.heightAnchor.constraint(equalToConstant: 206),
+            scrollView.topAnchor.constraint(equalTo: navigationBar.view.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 2),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -32), // Ограничение для ширины stackView
             
             tableView.heightAnchor.constraint(equalToConstant: 300),
             
+            
         ])
     }
+    
+    //MARK: - NavigationBar
+    private func setupNavBar() {
+        navigationBar.titleOfViewLabel.text = ""
+        navigationBar.view.translatesAutoresizingMaskIntoConstraints = false
+        addChild(navigationBar)
+        view.addSubview(navigationBar.view)
+        navigationBar.didMove(toParent: self)
+    }
+    
     
     //MARK: - TableView
     
@@ -135,5 +205,11 @@ class RecipeDetailView: UIViewController, UITableViewDataSource, UITableViewDele
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! CustomCellRecipe
+        cell.toggleCheckmark()
+    }
+    
 }
 
